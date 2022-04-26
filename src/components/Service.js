5519a96c771792439416e5e27452460d0a064355
Srijan67 <srijansinha67@gmail.com>
2022-04-26 19:45:47 +0530

    headArea slider

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,6 +1,6 @@
 import Slider from "react-slick"
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ExploreBtn from "../images/svg/exporebtn.svg"
 import UnityLogo from "../images/uintyimg.png"
 import Twitter from "../images/svg/twittter.svg"
